c56eef5d22f0f1125f5787fe3f86faae252205ec
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-07-14 12:57:15 -0600

    Changed User attribute 'name' to 'email'

--- app/graphql/types/pack_type.rb
+++ app/graphql/types/pack_type.rb
@@ -6,5 +6,7 @@ module Types
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
     field :owner_id, Integer, null: true
+    
+    field :users, [Types::UserType], null: true
   end
 end
