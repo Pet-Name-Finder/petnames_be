f55b74c46b058535cfb911af450982e9db1f10ff
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-07-14 17:45:36 -0600

    Add liked_names and pet_names fields

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -13,6 +13,35 @@ module Types
     def user(id:)
       User.find(id)
     end
-
+    
+    # /packs
+    field :packs, [Types::PackType], null: false
+    
+    def packs
+      Pack.all
+    end
+    field :pack, Types::PackType, null: false do
+      argument :id, ID, required: true
+    end
+    
+    # /liked_names
+    field :liked_names, [Types::LikedNameType], null: false
+    
+    def liked_names
+      LikedName.all
+    end
+    field :liked_name, Types::LikedNameType, null: false do
+      argument :id, ID, required: true
+    end
+    
+    # /pet_names
+    field :pet_names, [Types::PetNameType], null: false
+    
+    def pet_names
+      PetName.all
+    end
+    field :pet_name, Types::PetNameType, null: false do
+      argument :id, ID, required: true
+    end
   end
 end
