7a605abe43f5887ead52ef70f6e497d98db20085
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-07-17 13:16:33 -0600

    Add show all packs

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -6,13 +6,22 @@ module Types
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
     field :user_packs, [Types::UserPackType], null: true
+    field :packs, [Types::PackType], null: true
     field :liked_names, [Types::LikedNameType], null: true
     field :user_packs_count, Integer, null: true
+    field :show_all_packs, String, null: true
 
 
     def user_packs_count
       object.user_packs.count
     end
-
+    
+    def show_all_packs
+      require "pry";binding.pry   
+      object.user_pack.pack.name  ##Need to show list of all packs
+    end
   end
 end
+
+
+
