9076c74262204e10f51530c8d84efdbb2ef1332d
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-07-19 15:11:32 -0600

    Add all liked names endpoint

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -6,15 +6,21 @@ module Types
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
     field :liked_names, [Types::LikedNameType], null: true
+    field :user_liked_names, [Types::PetNameType], null: true
+    field :names_not_yet_viewed, [Types::LikedNameType], null: true
 
 
     def user_liked_names
-      object.liked_names.where(liked: true)
+      array = []
+      object.liked_names.where(liked: true).each do |liked_name|
+        array << liked_name.pet_name
+      end
+      array
     end
-    # 
-    # def user_not_yet_viewed_names
-    #   object.liked_names.where(liked: null)
-    # end
+    
+    def names_not_yet_viewed
+      object.liked_names.where(liked: null)
+    end
   end
 end
 
