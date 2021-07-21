ce6b0682f0ba453011d8d9fba790da74995bc2e9
Kseniya K <kseniya.kudelich@gmail.com>
2021-07-20 22:15:29 -0600

    Add create liked name

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :create_user, mutation: Mutations::CreateUser
     field :create_pet_name, mutation: Mutations::CreatePetName
+    field :create_liked_name, mutation: Mutations::CreateLikedName
   end
 end
